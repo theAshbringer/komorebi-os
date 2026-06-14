0d45a69c62b0c8dcb0374a2a06795e160b3a8521
Kristina Jumanova <kristya1310@gmail.com>
2026-06-14 17:13:05 +0700

    feat(resolvers): filter only several types of devices

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -4,11 +4,14 @@ export const resolvers = {
   Query: {
     devices: async (): Promise<HAState[]> => {
       const states = await fetchHAStates();
-      return states.map((state) => ({
-        entityId: state.entity_id,
-        state: state.state,
-        friendlyName: state.attributes.friendly_name || state.entity_id,
-      }));
+      const deviceTypes = ['sensor', 'switch', 'light', 'button'];
+      return states
+        .filter((state) => deviceTypes.includes(state.entity_id.split('.')[0]))
+        .map((state) => ({
+          entityId: state.entity_id,
+          state: state.state,
+          friendlyName: state.attributes.friendly_name || state.entity_id,
+        }));
     },
   },
 };
