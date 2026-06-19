c6cb635839dedc9c9af93a23c8c3b1a27036437a
Kristina Jumanova <kristya1310@gmail.com>
2026-06-19 23:28:06 +0700

    feat: set up apollo client

---
+++ src/lib/apollo/ApolloClient.ts
@@ -0,0 +1,20 @@
+import { env } from '@/lib/env';
+import { HttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  registerApolloClient,
+} from '@apollo/client-integration-nextjs';
+
+export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+      // Use an absolute URL for SSR (relative URLs cannot be used in SSR)
+      uri:
+        `${env.NEXT_PUBLIC_HOST_URL}/api/graphql` ||
+        'http://localhost:3000/api/graphql',
+      fetchOptions: {},
+    }),
+  });
+});
