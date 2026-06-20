9cb27571532e781e16b31f87eb2a22b487602cfa
Kristina Jumanova <kristya1310@gmail.com>
2026-06-20 20:45:20 +0700

    ref: delete comments

--- src/graphql/devices.ts
+++ src/graphql/devices.ts
@@ -1,4 +1,3 @@
-// src/graphql/devices.ts
 import { gql } from '@apollo/client';
 
 export const GET_DEVICES = gql`
--- src/lib/apollo/ApolloClient.ts
+++ src/lib/apollo/ApolloClient.ts
@@ -10,7 +10,6 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      // Use an absolute URL for SSR (relative URLs cannot be used in SSR)
       uri:
         `${env.NEXT_PUBLIC_HOST_URL}/api/graphql` ||
         'http://localhost:3000/api/graphql',
