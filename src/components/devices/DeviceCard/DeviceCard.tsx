5271aafe9b46feccd5faea83ecd99d8bbf103a90
Kristina Jumanova <kristya1310@gmail.com>
2026-06-29 20:23:03 +0700

    feat: isOn

--- src/components/devices/DeviceCard/DeviceCard.tsx
+++ src/components/devices/DeviceCard/DeviceCard.tsx
@@ -1,5 +1,6 @@
 import { Card } from '@/components/ui/card';
 import { Switch } from '@/components/ui/switch';
+import { cn } from '@/lib/utils';
 import { DeviceState, DeviceType } from '@/types/device';
 import { DeviceIcon } from './DeviceIcon';
 interface DeviceCardProps {
@@ -10,22 +11,25 @@ interface DeviceCardProps {
 
 export function DeviceCard({ name, type, state }: DeviceCardProps) {
   const room = 'Гостиная';
+  const isOn = state === 'on';
   return (
     <Card size="sm" className="@container h-46 w-42 p-4 shadow-lg/20">
       <div className="flex justify-between">
-        <div className="bg-muted flex size-[38cqw] shrink-0 items-center justify-center rounded-full">
+        <div
+          className={cn(
+            'bg-muted shadow-primary/30 flex size-[38cqw] shrink-0 items-center justify-center rounded-full',
+            isOn && 'shadow-md'
+          )}
+        >
           <DeviceIcon type={type} className="text-primary size-[64%]" />
         </div>
-        <Switch></Switch>
+        <Switch checked={isOn}></Switch>
       </div>
       <div className="mt-auto">
         <h3 className="truncate text-[clamp(1.1rem,13cqw,1.4rem)] font-bold">
           {name}
         </h3>
-        {state === 'on' && (
-          <p className="text-primary text-[10cqw] font-semibold">Включено</p>
-        )}
-        <p className="text-muted-foreground text-[9cqw] font-medium">{room}</p>
+        <p className="text-primary text-[10cqw] font-semibold">{room}</p>
       </div>
     </Card>
   );
